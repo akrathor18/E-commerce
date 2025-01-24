5a3e2d3620536990392dc5723514fc39f691ce66
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-01-24 21:41:52 +0530

    altering Admin panle

--- client/src/components/AdminPanel.jsx
+++ client/src/components/AdminPanel.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { UserCog } from 'lucide-react';
+import {React,useState } from "react";
+import { UserCog,Users,Plus,House,Shuffle , AlignJustify    } from 'lucide-react';
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 const AdminPanel = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <div className="text-text font-inter">
       {/* Sidebar */}
-      <div className="fixed left-0 top-0 w-64 h-full bg-primary p-4 z-50 sidebar-menu transition-transform">
-        <a href="#" className="flex items-center pb-4 border-b border-b-gray-800">
+      <div className={`fixed left-0 top-0 w-64 h-full bg-primary p-4 z-50  transition-transform ${sidebarOpen ? '' : 'hidden'} `}>
+        <a href="#" className={`flex items-center pb-4 border-b border-b-gray-400 `}>
           <h2 className="font-bold text-2xl">
            <img src={logo} alt="logo"/>
           </h2>
@@ -23,7 +24,8 @@ const AdminPanel = () => {
               className="flex font-semibold items-center py-2 px-4 text-text bg-secondary hover:text-hover rounded-md group-[.active]:bg-hover group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
             >
               <i className="ri-home-2-line mr-3 text-lg"></i>
-              <span className="text-sm">Dashboard</span>
+              <House/>
+              <span className="text-sm pl-3">Dashboard</span>
             </a>
           </li>
           <li className="mb-1 group">
@@ -32,7 +34,8 @@ const AdminPanel = () => {
               className="flex font-semibold items-center py-2 px-4 text-text bg-secondary hover:text-hover rounded-md group-[.active]:bg-hover group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle"
             >
               <i className="bx bx-user mr-3 text-lg"></i>
-              <span className="text-sm">Add Products</span>
+              <Plus />
+              <span className="text-sm pl-2">Add Products</span>
               <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
             </a>
            
@@ -43,7 +46,8 @@ const AdminPanel = () => {
               className="flex font-semibold items-center py-2 px-4 text-text bg-secondary hover:text-hover rounded-md group-[.active]:bg-hover group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle"
             >
               <i className="bx bx-user mr-3 text-lg"></i>
-              <span className="text-sm">Oders</span>
+               <Shuffle />
+              <span className="text-sm pl-3">Oders</span>
               <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
             </a>
            
@@ -54,7 +58,9 @@ const AdminPanel = () => {
               className="flex font-semibold items-center py-2 px-4 text-text bg-secondary hover:text-hover rounded-md group-[.active]:bg-hover group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle"
             >
               <i className="bx bx-user mr-3 text-lg"></i>
-              <span className="text-sm">Users</span>
+              <Users />
+              <span className="text-sm pl-3">
+                Users</span>
               <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
             </a>
            
@@ -63,14 +69,16 @@ const AdminPanel = () => {
         </ul>
       </div>
 
-      <div className="fixed top-0 left-0 w-full h-full bg-text z-40 md:hidden sidebar-overlay">hh</div>
 
       {/* Main Content */}
-      <main className="w-full md:w-[calc(100%-256px)] md:ml-64 bg-secondary min-h-screen transition-all main">
+      <main className={`  w-full ${sidebarOpen ? 'md:ml-64 md:w-[calc(100%-256px)]' : ''}    bg-secondary min-h-screen transition-all main`}>
         {/* Navbar */}
         <div className="py-2 px-6 bg-primary flex items-center shadow-md shadow-gray-900 sticky top-0 left-0 z-30">
-          <button type="button" className="text-lg text-gray-900 font-semibold sidebar-toggle">
+          <button 
+          onClick={() => setSidebarOpen(prevState => !prevState)}
+          type="button" className="text-lg text-text font-semibold sidebar-toggle">
             <i className="ri-menu-line"></i>
+            <AlignJustify/>
           </button>
 
           <ul className="ml-auto flex items-center">
@@ -81,7 +89,7 @@ const AdminPanel = () => {
 {/* ------------------------------------Profile dropdown */}
 <Menu as="div" className="relative ml-3">
   <div>
-    <MenuButton className="relative flex rounded-full bg-secondry text-2xl  ">
+    <MenuButton className="relative flex rounded-full bg-primary text-2xl  ">
       <span className="absolute -inset-1.5" />
       <span className="sr-only">Open user menu</span>
       <UserCog />
@@ -90,7 +98,7 @@ const AdminPanel = () => {
   </div>
   <MenuItems
     transition
-    className="absolute right-0 z-10 mt-2 w-52 origin-top-right rounded-md bg-secondary text-text py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+    className="absolute right-0 z-10 mt-2 w-52 origin-top-right rounded-md bg-primary text-text py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
   >
     <MenuItem>
       <a
