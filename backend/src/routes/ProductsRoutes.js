52fdeb71ab41a83458f5d8d7bc482c52e288ad24
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-02-12 12:53:14 +0530

    creating get api for products

--- backend/src/routes/ProductsRoutes.js
+++ backend/src/routes/ProductsRoutes.js
@@ -7,7 +7,7 @@ router.post('/addProducts', async (req, res) => {
     try {
         const product = req.body;
         console.log(product);
-        
+
         const newProduct = new Products(product);
         await newProduct.save();
         res.status(201).json(`Products added successfully`);
@@ -17,5 +17,16 @@ router.post('/addProducts', async (req, res) => {
         res.status(500).json(`Internal server error`)
     }
 })
+
+router.get('/getProducts', async (req, res) => {
+    try {
+        const products = await Products.find();
+        res.json(products);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(`Internal server error`)
+    }
+}
+)
 
 export default router;
