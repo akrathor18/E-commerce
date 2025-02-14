42a1a32b20d316e189dfb5ab96008844510304e4
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-02-14 19:21:42 +0530

    creating searching functionality

--- backend/src/routes/ProductsRoutes.js
+++ backend/src/routes/ProductsRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import Products from '../models/Products.js';
 import authMiddleware from '../Middleware/authMiddleware';
 
-router.post('/addProducts',authMiddleware,async (req, res) => {
+router.post('/addProducts', authMiddleware, async (req, res) => {
     try {
         const product = req.body;
         console.log(product);
@@ -29,5 +29,27 @@ router.get('/getProducts', async (req, res) => {
     }
 }
 )
+
+router.get("/search", async (req, res) => {
+    console.log(req.query)
+    try {
+        const { searchTerm } = req.query;
+            console.log(searchTerm)
+        if (!searchTerm) {
+            return res.status(400).json({ message: "Search term is required" });
+        }
+
+        // Case-insensitive search using regex
+        const products = await Products.find({
+            title: { $regex: searchTerm, $options: "i" }, // "i" makes it case-insensitive
+        });
+
+        res.status(200).json({ results: products });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
+});
+
 
 export default router;
