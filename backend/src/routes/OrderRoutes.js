b6fa6c29d2c6005d98c9b39f8a7b560f90d32634
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-02-25 19:30:31 +0530

    cancel route

--- backend/src/routes/OrderRoutes.js
+++ backend/src/routes/OrderRoutes.js
@@ -33,16 +33,16 @@ router.post('/addOrder', authMiddleware, VerifyJwtMiddleware, async (req, res) =
     }
     const newOrder = new Order({
       user: {
-        _id,  
+        _id,
         name,
         email,
         address,
       },
 
-      products: productDetails, 
+      products: productDetails,
       totalAmount,
     });
-    console.log("newOrder",newOrder)
+    console.log("newOrder", newOrder)
 
     const savedOrder = await newOrder.save();
     await User.findByIdAndUpdate(userId, { $push: { orders: savedOrder } });
@@ -66,14 +66,14 @@ router.get('/getOrders', authMiddleware, VerifyJwtMiddleware, async (req, res) =
   }
 })
 
-router.put('/product/:id', authMiddleware, VerifyJwtMiddleware, async (req, res)=>{
+router.put('/product/:id', authMiddleware, VerifyJwtMiddleware, async (req, res) => {
   try {
-    const id= req.params.id;
-    const { status } = req.body;  
+    const id = req.params.id;
+    const { status } = req.body;
     const order = await Order.findById(req.params.id);
 
-    if(!order){
-      res.status(404).json({massage:'Order not found!'})
+    if (!order) {
+      res.status(404).json({ massage: 'Order not found!' })
     }
     const validStatuses = ["Pending", "Shipped", "Delivered", "Cancelled"];
     if (!validStatuses.includes(status)) {
@@ -90,41 +90,61 @@ router.put('/product/:id', authMiddleware, VerifyJwtMiddleware, async (req, res)
   }
 })
 
-router.get('/', authMiddleware, VerifyJwtMiddleware, async (req, res)=>{
+router.get('/', authMiddleware, VerifyJwtMiddleware, async (req, res) => {
   try {
     const OrderProducts = await Order.find();
-        res.json(OrderProducts);
+    res.json(OrderProducts);
   } catch (error) {
     console.log(error)
-    res.statues(500).json({massage:"Internal sserver error"})
+    res.statues(500).json({ massage: "Internal sserver error" })
   }
 })
 
-router.get('/product/:id', authMiddleware, VerifyJwtMiddleware, async (req, res)=>{
+router.get('/product/:id', authMiddleware, VerifyJwtMiddleware, async (req, res) => {
   try {
-    const id= req.params.id;
+    const id = req.params.id;
     const productsDetail = await Order.findById(id);
-        res.json(productsDetail);
+    res.json(productsDetail);
   } catch (error) {
     console.log(error)
-    res.status(500).json({massage:"Internal sserver error"})
+    res.status(500).json({ massage: "Internal sserver error" })
   }
 })
 
-router.get('/user', authMiddleware, VerifyJwtMiddleware, async (req,res)=>{
+router.get('/user', authMiddleware, VerifyJwtMiddleware, async (req, res) => {
   try {
     const userId = req.user.id;
-  const orders= await Order.find({user:userId}).sort({createdAt:-1})
+    const orders = await Order.find({ user: userId }).sort({ createdAt: -1 })
 
-  if (!orders.length) {
-    return res.status(404).json({ message: "No orders found" });
-  }
+    if (!orders.length) {
+      return res.status(404).json({ message: "No orders found" });
+    }
 
-  res.status(200).json(orders)
-  
+    res.status(200).json(orders)
+
   } catch (error) {
     console.log(error)
-    req.status(500).json({massage:'Interna server error'})
+    req.status(500).json({ massage: 'Interna server error' })
+  }
+})
+
+router.put('/cancel/:orderId', authMiddleware, VerifyJwtMiddleware, async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ massage: 'Order not found!' })
+    }
+    if (order.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized to cancel this order" });
+    }
+    order.status = "Cancelled";
+    await order.save();
+
+    res.status(200).json({ message: "Order canceled successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ massage: "Internal Server error" });
   }
 })
 export default router
